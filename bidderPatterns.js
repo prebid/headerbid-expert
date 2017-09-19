8b4eeae6ae1cb3d42433abfc1a4e28b53e81ce79
Matt Lane <mlane@appnexus.com>
2017-09-19 08:53:23 -0700

    Initial commit

---
+++ bidderPatterns.js
@@ -0,0 +1,279 @@
+var bidderPatterns = {
+  'AppNexus': [
+    '.adnxs.com/jpt',
+    '.adnxs.com/ut*/prebid'
+  ],
+  'Openx': [
+    '.servedbyopenx.com/w/1.0/acj',
+    '.openx.net/w/1.0/acj'
+  ],
+  'Pubmatic': [
+    '.pubmatic.com/AdServer/AdCallAggregator',
+    '.pubmatic.com/AdServer/AdServerServlet'
+  ],
+  'Rubicon': [
+    '.rubiconproject.com/a/api/fastlane.json',
+    '.rubiconproject.com/a/api/ads.json'
+  ],
+  'Criteo': [
+    '.criteo.com/delivery/rta/rta.js',
+    'bidder.criteo.com/'
+  ],
+  'Conversant': [
+    'media.msg.dotomi.com/s2s/header'
+  ],
+  'Amazon': [
+    '.amazon-adsystem.com/e/dtb/bid'
+  ],
+  'AOL': [
+    '.adtechus.com/pubapi*cmd=bid',
+    '.adtech.advertising.com/pubapi*cmd=bid',
+    '.adtech.de/pubapi*cmd=bid',
+    '.adtechjp.com/pubapi*cmd=bid'
+  ],
+  'Sovrn': [
+    '.lijit.com/rtb/bid'
+  ],
+  'Yieldbot': [
+    '.yldbt.com/m/'
+  ],
+  'Sonobi': [
+    '.sonobi.com/trinity.js'
+  ],
+  'Index': [
+    '.casalemedia.com/cygnus',
+    '.casalemedia.com/headertag'
+  ],
+  'Proximic': [
+    '.zqtk.net/'
+  ],
+  'AudienceS': [
+    '.revsci.net/pql'
+  ],
+  'Adform': [
+    'adx.adform.net/adx/?rp=4'
+  ],
+  'WideOrbit': [
+    '.atemda.com/JSAdservingMP.ashx'
+  ],
+  'PulsePoint': [
+    'bid.contextweb.com/header'
+  ],
+  'Roxot': [
+    'r.rxthdr.com/'
+  ],
+  'Komoona': [
+    's.komoona.com/',
+    'bidder.komoona.com/v1/GetSBids'
+  ],
+  'Widespace': [
+    'engine.widespace.com/'
+  ],
+  'Mantis': [
+    'mantodea.mantisadnetwork.com/'
+  ],
+  'SmartRTB+': [
+    'prg.smartadserver.com/prebid'
+  ],
+  'RhythmOne': [
+    'tag.1rx.io/rmp/*/*/mvo'
+  ],
+  'DFP': [
+    '.doubleclick.net/gampad/ads'
+  ],
+  'Aardvark': [
+    'thor.rtk.io/*/*/aardvark'
+  ],
+  'Adblade': [
+    'rtb.adblade.com/prebidjs/bid'
+  ],
+  'AdBund': [
+    'us-east-engine.adbund.xyz/prebid/ad/get',
+    'us-west-engine.adbund.xyz/prebid/ad/get'
+  ],
+  'AdButler': [
+    'servedbyadbutler.com/adserve/;type=hbr;'
+  ],
+  'Adequant': [
+    'rex.adequant.com/rex/c2s_prebid?'
+  ],
+  'AdKernel': [
+    'cpm.metaadserving.com/rtbg'
+  ],
+  'AdMedia': [
+    'b.admedia.com/banner/prebid/bidder/?'
+  ],
+  'Admixer': [
+    'inv-nets.admixer.net/prebid.aspx'
+  ],
+  'Bidfluence': [
+    'bidfluence.azureedge.net/forge.js'
+  ],
+  'Brightcom': [
+    'hb.iselephant.com/auc/ortb'
+  ],
+  'Centro': [
+    '.brand-server.com/hb'
+  ],
+  'DistrictM': [
+    'prebid.districtm.ca/lib.js'
+  ],
+  'Fidelity': [
+    'x.fidelity-media.com/delivery/hb.php?'
+  ],
+  'GetIntent': [
+    'cdn.adhigh.net/adserver/hb.js'
+  ],
+  'GumGum': [
+    'g2.gumgum.com/hbid/imp'
+  ],
+  'HIRO Media': [
+    'hb-rtb.ktdpublishers.com/bid/get'
+  ],
+  'JCM': [
+    'media.adfrontiers.com/pq?t=hb&bids='
+  ],
+  'Komoona': [
+    'bidder.komoona.com/v1/GetSBids'
+  ],
+  'Lifestreet': [
+    'ads.lfstmedia.com/getad?'
+  ],
+  'Meme Global': [
+    'stinger.memeglobal.com/api/v1/services/prebid'
+  ],
+  'NginAd': [
+    'server.nginad.com/bid/rtb?'
+  ],
+  'Piximedia': [
+    'static.adserver.pm/prebid'
+  ],
+  'Sekindo': [
+    'hb.sekindo.com/live/liveView.php?'
+  ],
+  'ServerBid': [
+    'e.serverbid.com/api/v2'
+  ],
+  'Sharethrough': [
+    'btlr.sharethrough.com/header-bid/v1?'
+  ],
+  'SmartyAds': [
+    'ssp-nj.webtradehub.com/?'
+  ],
+  'SpringServe': [
+    'bidder.springserve.com/display/hbid?'
+  ],
+  'StickyAdsTV': [
+    'cdn.stickyadstv.com/mustang/mustang.min.js',
+    'cdn.stickyadstv.com/prime-time/intext-roll.min.js',
+    'cdn.stickyadstv.com/prime-time/screen-roll.min.js'
+  ],
+  'TapSense': [
+    'ads04.tapsense.com/ads/headerad'
+  ],
+  'ThoughtLeadr': [
+    'cdn.thoughtleadr.com/v4'
+  ],
+  'TripleLift': [
+    'tlx.3lift.com/header/auction?'
+  ],
+  'Twenga': [
+    'rtb.t.c4tw.net/Bid?'
+  ],
+  'Underdog Media': [
+    'udmserve.net/udm/img.fetch?tid=1;dt=9;'
+  ],
+  'Vertamedia': [
+    'rtb.vertamedia.com/hb'
+  ],
+  'Vertoz': [
+    'banner.vrtzads.com/vzhbidder/bid?'
+  ],
+  'Xhb': [
+    'ib.adnxs.com/jpt?'
+  ],
+  'Atomx': [
+    'p.ato.mx/placement'
+  ],
+  'Facebook': [
+    'an.facebook.com/v2/placementbid.json'
+  ],
+  'Inneractive': [
+    'ad-tag.inner-active.mobi/simpleM2M/requestJsonAd'
+  ],
+  'Innity': [
+    'as.innity.com/synd/?cb='
+  ],
+  'Beachfront': [
+    'reachms.bfmio.com/bid.json?exchange_id='
+  ],
+  'HuddledMasses': [
+    'huddledmassessupply.com/?banner_id='
+  ],
+  'Trion': [
+    'in-appadvertising.com/api/bidRequest?'
+  ],
+  'Pubgears': [
+    'b.pubgears.com/nucleus/request'
+  ],
+  'AdSupply': [
+    'engine.4dsply.com/banner.engine?id='
+  ],
+  'Quantcast': [
+    'global.qc.rtb.quantserve.com/qchb'
+  ],
+  'Adyoulike': [
+    'hb-api.omnitagjs.com/hb-api/prebid'
+  ],
+  'Carambola': [
+    'hb.carambo.la/hb'
+  ],
+  'Cox': [
+    'ad.afy11.net/ad'
+  ],
+  'EPlanning': [
+    '.e-planning.net/layers/t_pbjs_'
+  ],
+  'Unruly': [
+    'targeting.unrulymedia.com/prebid'
+  ],
+  'Spotx': [
+    'search.spotxchange.com/openrtb'
+  ],
+  'ucfunnel': [
+    'agent.aralego.com/header?'
+  ],
+  'Kargo': [
+    'krk.kargo.com/api/v1/bid'
+  ],
+  'C1X': [
+    'ht-integration.c1exchange.com/ht'
+  ],
+  'MobFox': [
+    'my.mobfox.com/request.php'
+  ],
+  'imonomy': [
+    'b.imonomy.com/openrtb/hb'
+  ],
+  'Justpremium': [
+    'cdn-cf.justpremium.com/js'
+  ],
+  'Marsmedia': [
+    'bid306.rtbsrv.com/bidder/?bid=3mhdom'
+  ],
+  'Essens': [
+    'bid.essrtb.com/bid/prebid_call'
+  ],
+  'Platform.io': [
+    'js.adx1.com/pb_ortb.js?cb='
+  ],
+  'Pollux': [
+    'adn.plxnt.com/prebid'
+  ],
+  'FeatureForward': [
+    'prmbdr.featureforward.com/newbidder/bidder1_prm.php?'
+  ],
+  'Prebid Server': [
+    'prebid.adnxs.com/pbs/v1/auction'
+  ],
+};
